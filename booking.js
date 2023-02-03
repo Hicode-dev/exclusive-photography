7f52cb562bf24b6eab2b8fc39673d11dfc9afb9c
hicode-dev <kingdavies2021@gmail.com>
2023-02-02 23:08:26 -0800

    first commit

---
+++ booking.js
@@ -0,0 +1,7 @@
+let other = document.getElementById('nob')
+other.style.display= 'none'
+
+function op(params) {
+other.style.display= 'block'
+    
+}
--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-function about(params) {
-    let ab = document.getElementById('aboutUs')
-    console.log('weoojip');
-    
-ab.scrollIntoView(true)
-    }
+const header = document.querySelector("header");
 
-function scrolltoId(){
-    window.scrollTo({
-    top: access.scrollTop,
-    left: access.scrollLeft});
-    }
-    $("a[href^='#']").click(function(e) {
-        e.preventDefault();
-        
-        var position = $($(this).attr("href")).offset().top;
-    
-        $("body, html").animate({
-            scrollTop: position
-        } /* speed */ );
-    });
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('active');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+	distance: '45px',
+	duration: 2700,
+	reset: true
+})
+
+sr.reveal('.home-text',{delay:350, origin:'left'});
+sr.reveal('.home-img',{delay:350, origin:'right'});
+
+sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'});
+
+let myArray = [ './images/WhatsApp Image 2022-11-02 at 1.23.10 PM.jpeg ','./images/WhatsApp Image 2022-11-02 at 1.23.10 PM.jpeg ','./images/WhatsApp Image 2022-11-02 at 1.23.10 PM.jpeg ','./images/WhatsApp Image 2022-11-02 at 1.23.10 PM.jpeg ' ]
+
+const help = document.getElementsByClassName('pict')
+
+
+// for (let index = 0; index < pict.length; index++) {
+// 	const element = pict[index];
+// 	console.log(element);
+
+	
+// }
+help.forEach(element => {
+	console.log(element);
+});
+
+for (let i = 0; i < myArray.length; i++) {
+	const element = array[i];
+
+
+
+	
+}
+
+
